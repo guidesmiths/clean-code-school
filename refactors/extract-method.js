16f61ef01a163f6eecdc78dbb89396250607b2ae
feliun <felipe.polo@gmail.com>
2019-01-09 15:56:53 +0100

    Extract method refactor

---
+++ refactors/extract-method.js
@@ -0,0 +1,49 @@
+/*
+A code fragment can be grouped together
+Wrap the fragment in a new method and lets its name explain the intent
+*/
+
+const data = {
+	address: {
+		street: 'calle de la Alameda',
+		zipCode: '28014',
+	},
+	person: {
+		name: 'Thomas',
+		lastName: 'Anderson',
+	},
+};
+
+const before = () => {
+	const printAddress = () => {
+		console.log(`Street ${data.address.street}`);
+		console.log(`ZIP Code ${data.address.zipCode}`);
+	};
+	return {
+		run: () => {
+			printAddress();
+			console.log(`First name: ${data.person.name}`);
+			console.log(`Last name: ${data.person.lastName}`);
+		},
+	};
+};
+
+const after = () => {
+	const printAddress = () => {
+		console.log(`Street ${data.address.street}`);
+		console.log(`ZIP Code ${data.address.zipCode}`);
+	};
+	const printPerson = () => {
+		console.log(`First name: ${data.person.name}`);
+		console.log(`Last name: ${data.person.lastName}`);
+	};
+	return {
+		run: () => {
+			printAddress();
+			printPerson();
+		},
+	};
+};
+
+before().run();
+after().run();
