d60a04adad96c671618f2ce7816350e82ed0a56f
feliun <felipe.polo@gmail.com>
2019-01-18 18:32:52 +0100

    Refactor: each technique inside their own folder

---
+++ refactors/extract-method/before.js
@@ -0,0 +1,14 @@
+/*
+A code fragment can be grouped together
+Wrap the fragment in a new method and lets its name explain the intent
+*/
+
+const data = require('./data.json');
+
+const printAddress = () => {
+  console.log(`Street ${data.address.street}`);
+  console.log(`ZIP Code ${data.address.zipCode}`);
+};
+printAddress();
+console.log(`First name: ${data.person.name}`);
+console.log(`Last name: ${data.person.lastName}`);
