1e2e987c28f1a56241c9fa398da53ff2ee6506d7
Fer <fer@MBP-de-Fernando.home>
2019-10-04 15:47:39 +0200

    clean lint and spaces

--- code-smells/feature-envy/after.js
+++ code-smells/feature-envy/after.js
@@ -1,8 +1,5 @@
 const { afterPrinter } = require('./helper');
 
-// MOVE METHOD
-// implements getFormatPhone moved from makeUSer
-// implements private members
 function makePhone(unformattedNumber) {
     this.phone = unformattedNumber;
     this.chunkSize = 3;
@@ -17,8 +14,6 @@ function makePhone(unformattedNumber) {
     }
 }
 
-// makeUser method has single responsibility. 
-// makerUser method is uncoupling.
 function makeUser(name, lastName, dni, phone, email) {
     this.name = name;
     this.lastName = lastName;
@@ -47,12 +42,7 @@ const user1 = makeUser('Fernando', 'Aparicio Galende', '12345678S', formatedNumb
 
 afterPrinter(user1, phone1)
 
-
-// //////////    --- BONUS ---     ////////////
 console.log(`\nThe private member gets undefined: ${user1.phone}`);
 
 delete user1.phone;
 console.log(`\nThe private member can't delete: ${user1.getPhone()}`);
-
-
-
