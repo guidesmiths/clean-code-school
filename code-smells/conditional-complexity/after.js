84dd32d1be949f36e0c89cf99ad1fba23a5c970c
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-05-16 21:06:29 +0200

    feat(after): add after.js

---
+++ code-smells/conditional-complexity/after.js
@@ -0,0 +1,10 @@
+/* Extract method */
+
+const getDogEmoji = () => '🐶';
+const getCatEmoji = () => '🐱';
+const getFrogEmoji = () => '🐸';
+const getPandaEmoji = () => '🐼';
+const getGiraffeEmoji = () => '🦒';
+const getMonkeyEmoji = () => '🐵';
+const getUnicornEmoji = () => '🦄';
+const getDragonEmoji = () => '🐲';
