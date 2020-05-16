a95509217dd2b3cb365bccd7e24f5f9ab4d91fcf
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-05-16 21:06:18 +0200

    feat(before): add before.js

---
+++ code-smells/conditional-complexity/before.js
@@ -0,0 +1,19 @@
+const getAnimalEmoji = animal => {
+  if (animal === 'dog') {
+    return '🐶';
+  } else if (animal === 'cat') {
+    return '🐱';
+  } else if (animal === 'frog') {
+    return '🐸';
+  } else if (animal === 'panda') {
+    return '🐼';
+  } else if (animal === 'giraffe') {
+    return '🦒';
+  } else if (animal === 'monkey') {
+    return '🐵';
+  } else if (animal === 'unicorn') {
+    return '🦄';
+  } else if (animal === 'dragon') {
+    return '🐲';
+  }
+};
