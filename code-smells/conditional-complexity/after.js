b8258d138207d4cfa4e9be4ec7f32eba015e30de
reymon359 <ramonmorcillocascales@gmail.com>
2020-05-21 16:52:54 +0200

    feat: add after early exit

--- code-smells/conditional-complexity/after.js
+++ code-smells/conditional-complexity/after.js
@@ -23,3 +23,23 @@ const printMyAnimal = (animal) => {
 	}
 };
 console.log(printAnimals('hamster'));
+
+/* After Early exit / Return early 
+
+We can refactor printAnimalDetails with ternary operators, 
+&& conditions, etc. but instead let's write more precise code 
+by using multiple return statements. */
+
+const printAnimalDetails = ({ type, name, gender } = {}) => {
+	if (!type) return 'No animal type';
+	if (!name) return 'No animal name';
+	if (!gender) return 'No animal gender';
+
+	return `${name} is a ${gender} ${type}`;
+};
+console.log(printAnimalDetails()); // 'No animal type'
+console.log(printAnimalDetails({ type: 'dog' })); // 'No animal name'
+console.log(printAnimalDetails({ type: 'dog', gender: 'female' })); // 'No animal name'
+console.log(
+	printAnimalDetails({ type: 'dog', name: 'Lucy', gender: 'female' })
+); // 'Lucy is a female dog'
