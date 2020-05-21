d907a501886c285d48fc0af2ff42516994476398
reymon359 <ramonmorcillocascales@gmail.com>
2020-05-21 16:38:25 +0200

    feat: add after array includes

--- code-smells/conditional-complexity/after.js
+++ code-smells/conditional-complexity/after.js
@@ -8,5 +8,18 @@ const getGiraffeEmoji = () => '🦒';
 const getMonkeyEmoji = () => '🐵';
 const getUnicornEmoji = () => '🦄';
 const getDragonEmoji = () => '🐲';
-
 console.log(getFrogEmoji());
+
+/* After Array.includes 
+
+We can rewrite the conditional 
+above by using Array.includes */
+
+const printMyAnimal = (animal) => {
+	const animals = ['dog', 'cat', 'hamster', 'turtle'];
+
+	if (animals.includes(animal)) {
+		console.log(`I have a ${animal}`);
+	}
+};
+console.log(printAnimals('hamster'));
--- code-smells/conditional-complexity/before.js
+++ code-smells/conditional-complexity/before.js
@@ -1,30 +1,34 @@
 /* Before using Extract Method */
 
-const getAnimalEmoji = animal => {
-  if (animal === 'dog') {
-    return '🐶';
-  } else if (animal === 'cat') {
-    return '🐱';
-  } else if (animal === 'frog') {
-    return '🐸';
-  } else if (animal === 'panda') {
-    return '🐼';
-  } else if (animal === 'giraffe') {
-    return '🦒';
-  } else if (animal === 'monkey') {
-    return '🐵';
-  } else if (animal === 'unicorn') {
-    return '🦄';
-  } else if (animal === 'dragon') {
-    return '🐲';
-  }
+const getAnimalEmoji = (animal) => {
+	if (animal === 'dog') {
+		return '🐶';
+	} else if (animal === 'cat') {
+		return '🐱';
+	} else if (animal === 'frog') {
+		return '🐸';
+	} else if (animal === 'panda') {
+		return '🐼';
+	} else if (animal === 'giraffe') {
+		return '🦒';
+	} else if (animal === 'monkey') {
+		return '🐵';
+	} else if (animal === 'unicorn') {
+		return '🦄';
+	} else if (animal === 'dragon') {
+		return '🐲';
+	}
 };
 console.log(getAnimalEmoji('dragon'));
 
-/* Before Array.includes */
-const printMyAnimal = animal => {
-  if (animal === 'dog' || animal === 'cat') {
-    console.log(`I have a ${animal}`);
-  }
-}
-console.log(printMyAnimal('dog'))
+/* Before Array.includes 
+
+If we will keep extending the statement with more 
+OR statements, the code will get harder to maintain */
+
+const printMyAnimal = (animal) => {
+	if (animal === 'dog' || animal === 'cat') {
+		console.log(`I have a ${animal}`);
+	}
+};
+console.log(printMyAnimal('dog'));
